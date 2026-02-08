49823cc1b734c97acf4233cd3c536ad0e63c0492
Roanna Victorio <roannav@users.noreply.github.com>
2026-02-07 17:50:44 -0800

    change color of title for default theme, for better contrast in dark and light mode

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 export const COLORS: { [name: string]: Theme } = {
   default: {
     BACKGROUND: "#FFF",
-    TITLE: "#000",
+    TITLE: "#0366d6",
     ICON_CIRCLE: "#FFF",
     TEXT: "#666",
     LAUREL: "#009366",
